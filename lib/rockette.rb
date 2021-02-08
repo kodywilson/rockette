58aca4654c6d5d4b39f8781095407d78b1893ccc
Kody Wilson <kodywilson@gmail.com>
2021-02-08 11:10:13 -0800

    Initial commit.

---
+++ lib/rockette.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rockette/version"
+
+module Rockette
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rockette/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Rockette
+  VERSION = "0.1.0"
+end
