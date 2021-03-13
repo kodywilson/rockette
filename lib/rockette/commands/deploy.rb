09b94d5dd850a67ed55407ef0f4c5d4ac1614bbf
Kody Wilson <kodywilson@gmail.com>
2021-03-13 08:04:57 -0800

    remove unused code

--- lib/rockette/commands/deploy.rb
+++ lib/rockette/commands/deploy.rb
@@ -9,21 +9,6 @@ module Rockette
         @options = options
       end
 
-       # Import application export into APEX instance
-      def import_app(url, body)
-        response = RestClient.post url, body
-      rescue StandardError => e
-        puts "Unable to import application because #{e.message}" # puts error
-      end
-
-      # Push attachment to api endpoint and show code
-      def push_blob(filey, headers, url)
-        response = RestClient.post url, filey, headers
-        response
-      rescue StandardError => e
-        puts "Unable to push file because #{e.message}" # puts error
-      end
-
       def execute(input: $stdin, output: $stdout)
         # Create and download export
         output.puts "OK, deploying export file #{@options[:file]}"
@@ -33,7 +18,6 @@ module Rockette
         push_hdrs["file_name"] = filey
         push_url = @options[:url] + 'data_loader/blob'
         pusher = Rester.new(headers: push_hdrs, meth: 'Post', params: File.open(filey), url: push_url).rest_try
-        #pusher = push_blob(File.open(filey), push_hdrs, push_url)
         # If push was successful, request application import
         unless pusher == nil
           if pusher.code == 200 || pusher.code == 201
@@ -45,7 +29,6 @@ module Rockette
             #body["app_id_tgt"] = "0" #test app copy
             url = @options[:url] + 'deploy/app'
             deploy = Rester.new(meth: 'Post', params: body, url: url).rest_try
-            #deploy = import_app(@options[:url] + 'deploy/app', body)
             unless deploy == nil
               puts deploy.code
               puts deploy.headers
